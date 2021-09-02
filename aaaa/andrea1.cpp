aeca483168d4da7f4e02330585c6711e67d817eb
Guilherme <82243438+BodnarGuilherme@users.noreply.github.com>
2021-09-01 22:14:52 -0300

    Add files via upload

--- aaaa/andrea1.cpp
+++ aaaa/andrea1.cpp
@@ -8,7 +8,6 @@ int main () {
     float areaA;
     float areaB;
 
-
     cout<<"Determine o valor do lado de um triangulo equilatero, triangulo A. \n";
     cin>> ladoA;
     cout<<"Determine o valor do lado de um triangulo equilatero, triangulo B. \n";
@@ -17,13 +16,12 @@ int main () {
     areaA = ladoA * 90 / 2;
     areaB = ladoB * 90 /2;
 
-
     if (areaA > areaB)
     {
         cout<<"A area do triangulo A, e a maior. \n";   
     }else{
         cout<<"A area do triangulo B, e a maior. \n";
     }
-    
-    
+    system("Pause");
+    return 0;    
 }
