fe094de977ffa1b2d33dce1eeeea6d042a5f106e
Guilherme <82243438+BodnarGuilherme@users.noreply.github.com>
2021-09-01 22:01:57 -0300

    Add files via upload

---
+++ aaaa/andrea1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+    float ladoA;
+    float ladoB;
+    float areaA;
+    float areaB;
+
+    //q puta enunciado mal feito do krlh
+
+    cout<<"Determine o valor do lado de um triangulo equilatero, triangulo A. \n";
+    cin>> ladoA;
+    cout<<"Determine o valor do lado de um triangulo equilatero, triangulo B. \n";
+    cin>> ladoB;
+
+    areaA = ladoA * 90 / 2;
+    areaB = ladoB * 90 /2;
+
+    //já mostrou a area nessa poha
+
+    if (areaA > areaB)
+    {
+        cout<<"A area do triangulo A, e a maior. \n";   
+    }else{
+        cout<<"A area do triangulo B, e a maior. \n";
+    }
+    
+    
+}
